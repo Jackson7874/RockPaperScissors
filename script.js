0e76549bd92d887741638d836f0c9eb9736422da
Jacksono7874 <Jacksono7874@gmail.com>
2021-03-18 01:50:37 -0600

    Functional 5 rounds with inputs and logs to console

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const choices = ["rock", "paper", "scissors"]
+/*const choices = ["rock", "paper", "scissors"]
 
 function computerPlay() {
     const computerChoice = Math.floor(Math.random() * choices.length);
@@ -24,12 +24,82 @@ function playRound(playerSelection, computerSelection) {
   }
 }
 
-let playerSelection = prompt("Choose rock, paper, or scissors")
+let playerSelection = prompt("Choose rock, paper, or scissors").toLowerCase();
 const computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection));
+console.log(playRound(playerSelection, computerSelection)); */
 
 
 
-function game() {
 
+
+const choices = ["rock", "paper", "scissors"];
+let playerChoice;
+let computerChoice;
+let computerSelection;
+let playerSelection;
+let playerScore = 0;
+let computerScore = 0;
+
+function computerPlay() {
+  computerChoice = Math.floor(Math.random() * choices.length);
+  return choices[computerChoice];
 }
+
+function playerPrompt() {
+  playerChoice = prompt("Choose Rock, Paper, or Scissors").toLowerCase();
+  return playerChoice;
+}
+
+playerSelection = playerPrompt();
+
+function gameInfo() {
+  console.log("--------------------------")
+  console.log("Computer Score: " + computerScore);
+  console.log("Player Score: " + playerScore);
+}
+
+function playRound(playerSelection, computerSelection) {
+  computerSelection = computerPlay();
+  console.log("Computer Chose: " + choices[computerChoice]);
+  console.log("Player Chose: " + playerChoice);
+  if (playerSelection == computerSelection) {
+    console.log("--------------------------");
+    console.log("Tie game.");
+    gameInfo();
+    return;
+  } else if ((playerSelection == "rock" && computerSelection == "scissors") || (playerSelection == "scissors" && computerSelection == "paper") || (playerSelection == "paper" && computerSelection == "rock")) {
+    playerScore = ++playerScore;
+    console.log("--------------------------");
+    console.log("You win!");
+    gameInfo();
+    return playerScore;
+  } else {
+    computerScore = ++computerScore;
+    console.log("--------------------------");
+    console.log("You lose.");
+    gameInfo();
+    return computerScore;
+  }
+}
+
+// In the loop the first input doesn't display
+function game() {
+  let currentRound;
+  for (currentRound = 1; currentRound <= 5; currentRound++) {
+    let roundText = ("Round " + currentRound);
+    playerPrompt()
+    console.log("__________________________")
+    console.log(roundText)
+    console.log("__________________________")
+    playRound(playerSelection, computerSelection);
+  }
+  if (playerScore == computerScore) {
+    console.log("You tied the game against the computer.")
+  } else if (playerScore > computerScore) {
+    console.log("You won the game against the computer!")
+  } else {
+    console.log("You lost the game against the computer.")
+  }
+}
+
+game()
