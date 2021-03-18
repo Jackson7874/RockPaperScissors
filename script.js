ad73efec5219eecc5317445d8dc6e7d1f36a8e4b
Jacksono7874 <Jacksono7874@gmail.com>
2021-03-18 00:25:15 -0600

    One round playable version

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
 const choices = ["rock", "paper", "scissors"]
-const playerSelection = "rock";
-const computerSelection = computerPlay();
 
 function computerPlay() {
     const computerChoice = Math.floor(Math.random() * choices.length);
-    console.log(choices[computerChoice])
+    console.log("computer chose: " + choices[computerChoice])
+    return choices[computerChoice]
 }
 
 function playRound(playerSelection, computerSelection) {
-  if (playerSelection == "rock" && computerSelection == "scissors") {
-      return ("You Win! Paper beats Scissors.")
+  if (playerSelection == computerSelection) {
+    return "Tie game!"
+  } else if (playerSelection == "rock" && computerSelection == "paper") {
+    return "You lose!"
+  } else if (playerSelection == "rock" && computerSelection == "scissors") {
+    return "You win!"
   }
 }
 
-
+let playerSelection = "rock";
+const computerSelection = computerPlay();
 console.log(playRound(playerSelection, computerSelection));
